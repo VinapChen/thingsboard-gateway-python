f0d8b187665c4326c6093aa35b6d689f820620bb
Yunba <chenwenpei@yunba.io>
2018-12-25 18:08:23 +0800

    publish attributes and telemetry

--- mqttc.py
+++ mqttc.py
@@ -4,6 +4,7 @@
 import paho.mqtt.client as mqtt
 import json
 import os
+import time
 
 esl_gateway = ""
 esl_devices = []
@@ -21,14 +22,12 @@ def on_connect_tb(client_tb, userdata, flags, rc):
 
 def on_message_tb(client_tb, userdata, msg):
     print(msg.topic+" " + ":" + str(msg.payload,encoding = "utf-8"))
-
+    client_tb.subscribe([("v1/gateway/rpc", 0), ("v1/gateway/attributes", 0),("v1/gateway/attributes/response",0)])
 
 
 def on_publish_tb(topic, payload, qos):
     print("pub:",topic,payload,qos)
-    # Topic: v1 / gateway / connect
-    # Message: {"device": "Device A"}
-    client_tb.publish(topic, json.dumps({"device":payload}), qos)
+    client_tb.publish(topic, payload, qos)
     # client_tb.disconnect()
 
 
@@ -36,7 +35,15 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code t2:"+str(rc))
     client.subscribe("kbeacon/publish/D03304000652")
 
+message = {}
+telemetry_message = {}
 def on_message(client, userdata, msg):
+    global message
+    global telemetry_message
+
+    message = {}
+    telemetry_message = {}
+
     print(msg.topic+" " + ":" + str(msg.payload,encoding = "utf-8"))
     msg_data = str(msg.payload,encoding = "utf-8")
     d = eval(msg_data)
@@ -46,14 +53,48 @@ def on_message(client, userdata, msg):
     print(len(d["obj"]))
 
     for i in range(len(d["obj"])):
+        # add new device
+        # Topic: v1 / gateway / connect
+        # Message: {"device": "Device A"}
         if d["obj"][i]["dmac"] != "" and d["obj"][i]["dmac"] not in esl_devices:
             print(d["obj"][i]["dmac"])
             esl_devices.append(d["obj"][i]["dmac"])
+            on_publish_tb("v1/gateway/connect",json.dumps({"device":d["obj"][i]["dmac"]}),0)
 
-            # cmd = "mosquitto_pub -h 192.168.2.176 -p 1883 -t \"v1/gateway/connect\"  -m '{\"device\":\""+ d["obj"][i]["dmac"] +"\"}' -u \"H3wn8ScdYAiFPurZVsv4\""
-            # os.system(cmd)
-            on_publish_tb("v1/gateway/connect",d["obj"][i]["dmac"],0)
+        #    pub attributes data
+        # Topic: v1 / gateway / attributes
+        # Message: {"Device A": {"attribute1": "value1", "attribute2": 42},
+        #           "Device B": {"attribute1": "value1", "attribute2": 42}}
+        batteryV = float(int(d["obj"][i]["data1"][30:34],16)/1000)   #BYTE 15,16
+        temperature = float(int(d["obj"][i]["data1"][34:36],16)) + float(int(d["obj"][i]["data1"][36:38],16)/100)   #BYTE 17,18
+        data = {
+            d["obj"][i]["dmac"]:{"RSSI":d["obj"][i]["rssi"],
+                                 "TYPE":d["obj"][i]["data1"][22:24],    #BYTE 11
+                                 "FWVersion":d["obj"][i]["data1"][24:26],   #BYTE 12
+                                 "Ability":d["obj"][i]["data1"][26:28],     #BYTE 13,    14 reserve
+                                 "BattVolt":batteryV,
+                                 "Temperature":temperature,
+                                 "PictureID":d["obj"][i]["data1"][38:46],    #BYTE 19, 20, 21, 22
+                                 }
+        }
+        message = dict(message, **data)
 
+        #     Topic: v1/gateway/telemetry
+        telemetry_data = {
+            d["obj"][i]["dmac"]: [
+                {
+                    "ts": int(round(time.time() * 1000)),
+                    "values": {
+                        "temperature": temperature,
+                        "batteryV": batteryV
+                    }
+                }
+            ]
+        }
+        telemetry_message = dict(telemetry_message, **telemetry_data)
+
+    on_publish_tb("v1/gateway/attributes", json.dumps(message), 0)
+    on_publish_tb("v1/gateway/telemetry",json.dumps(telemetry_message),0)
 
     print(esl_devices)
 
@@ -79,10 +120,15 @@ client_tb.loop_start()
 # device = 'test\" -u \"H3wn8ScdYAiFPurZVsv4'
 # on_publish_tb('v1/gateway/connect', device, 0)
 
+# request attributes
+# Topic: v1/gateway/attributes/request
+# Message = {"id":"1112", "device": "2735000A33DD", "client": "true", "key": "BattVolt"}
+# on_publish_tb('v1/gateway/attributes/request', json.dumps(Message), 0)
+
 while True:
     device = input("device: ")
     # device = 'test\" -u \"H3wn8ScdYAiFPurZVsv4'
-    on_publish_tb('v1/gateway/connect',device,0)
+    on_publish_tb('v1/gateway/disconnect',device,0)
     if device == 'break':
         break
     pass
